982cfcb3b1aa06e0f16e97b9003fb7df42064b50
Chelsea Houston <chelseahouston29@gmail.com>
2021-01-21 13:37:52 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Count words in a Sentence.
+A Ruby program to count the number of words in a sentence.
+The user enters a sentence.
+The program responds with the number of words in a sentence.
+
+## Installation
+No installation required to run this project.
+```
+Ruby main.cpp
+```
+
+Created as part of TechTalent Academy Women in Software course.
---
+++ main.rb
@@ -0,0 +1,18 @@
+# How many words? ---
+puts "How many words are in your sentence?"
+print "Enter a sentence: "
+string01 = gets.chomp # user input sentence
+puts "Your sentence contains #{string01.split.size} words."
+
+# Sentence backwards
+puts "Your sentence backwards is:"
+puts string01.split.reverse.join(" ") 
+
+# More than one Sentence
+puts "How many sentences are in your paragraph?"
+print "Enter your paragraph: "
+string02 = gets.chomp # user input paragraph
+puts "Your paragraph has #{string02.scan(/[^\.!?]+[\.!?]/).map(&:strip).count} sentences."
+
+puts "Your paragraph backwards is:"
+puts string02.split.reverse.join(" ")
